d1dfb5f391c44b6e7bc3e061e50af7699838ef73
FatumaT <regretmakingthis@gmail.com>
2025-06-09 21:46:14 -0400

    The scholarships can be edited

--- scholarship.py
+++ scholarship.py
@@ -21,9 +21,9 @@ app = Flask(__name__)
 
 # These global variables are necessary because functions 
 # that control routes CAN'T have parameters
-scholarship_list = []
-num = 0
-scholarship_name = ''
+scholarship_list = [] # Holds all of our scholarship objects in the order they were inputted
+num = 0 # Helps in creating a unique ID for the scholarship
+edit_scholarship_name = '' # The name of the scholarship we are trying to edit
 
 class Scholarships:
     def __init__(self, identifier, name, amount, date_closed, category, date_open, link, priority, difficulty, notes):
@@ -37,6 +37,12 @@ class Scholarships:
         self.priority = priority
         self.difficulty = difficulty
         self.notes = notes
+
+    # Method that updates the scholarships attributes 
+    def update(self, new_name, new_amount, new_date):
+        self.name = new_name
+        self.amount = new_amount
+        self.date = new_date
 
 
 # Creates a unique identifier for each scholarship
@@ -80,28 +86,42 @@ def add_scholarship():
 
 # Stores the unqiue name of the scholarship being eddited
 @app.route("/get_name", methods=["GET", "POST"])
-def get_scholarship_name():
+def get_edit_scholarship_name():
     global scholarship_list
-    global scholarship_name
+    global edit_scholarship_name
     if request.method == "POST":
         # Stores the name of the scholarship
         # This tells update_scholarship which scholarship box to allow editing in
-        scholarship_name = request.form["scholarship_name"]
+        edit_scholarship_name = request.form["edit_scholarship_name"]
         # The HTML is then updated to become a form
         # TODO: Ensure only specific scholarship become a form
         return render_template(
             "index.html", 
             scholarships_list = scholarship_list,
-            scholarship_name = scholarship_name 
+            edit_scholarship_name = edit_scholarship_name 
         )
 
 # Takes in the information from the changed form
 @app.route("/edit", methods=["GET", "POST"])
 def update_scholarship():
-    global scholarship_name
+    global edit_scholarship_name
+    global scholarship_list
     name = str(request.form["new_name"])
     amount = float(request.form["new_amount"])
     due_date = str(request.form["new_due_date"])
+
+    scholarship_index = scholarship_list.index(edit_scholarship_name) # Finds the index that the
+    print(scholarship_index)
+    Scholarships.update_scholarship_object(scholarship_index, name, amount, due_date)
+    # We change the attributes of the scholarship
+    for scholarships in scholarship_list:
+        # If this scholarship is the one we want to edit
+        if scholarships.name == edit_scholarship_name:
+            # We edit the attributes of this object with the new ones
+            scholarships.update(name, amount, due_date)
+            edit_scholarship_name = '' # Empty this variable because we "edited" the scholarship
+            break # We no longer need to loop through the list
+    return redirect("/")
 
 # Allows for automatic updates on the flask browser while debugging
 if __name__ == "__main__":
